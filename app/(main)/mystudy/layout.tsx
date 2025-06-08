6000bceab8575069a2f9ccd00112a5c85eb127dc
B0XERCAT <seojin3154@naver.com>
2025-06-08 21:58:19 +0900

    refactor: remove MyStudyNavBar component from MyStudyLayout to streamline the layout structure

--- app/(main)/mystudy/layout.tsx
+++ app/(main)/mystudy/layout.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import MyStudyHeader from '@/components/mystudy/MyStudyHeader'
-import MyStudyNavBar from '@/components/mystudy/MyStudyNavBar'
 
 const MyStudyLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -9,7 +8,6 @@ const MyStudyLayout = ({ children }: { children: React.ReactNode }) => {
       <main className="w-full">
         <MyStudyHeader />
         <section className="mx-auto flex max-w-7xl px-4 pb-12 pt-[18px] max-sm:flex-col sm:px-8 sm:pb-16 sm:pt-8">
-          <MyStudyNavBar />
           {children}
         </section>
       </main>
